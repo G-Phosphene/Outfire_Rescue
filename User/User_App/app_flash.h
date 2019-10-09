e16b485c0afd237107e84744e7a15c72a744ffbb
C-Triones <1354869969@qq.com>
2019-10-09 10:08:50 +0800

    救援代码

--- User/User_App/app_flash.h
+++
@@ -1,7 +0,0 @@
-#ifndef _APP_FLASH_H
-#define _APP_FLASH_H
-
-void app_FlashWriteUdata(void);
-void app_FlashReadUdata(void);
-
-#endif
--- User/User_GUI/GUI_OLEDBmp.h
+++
@@ -1,4 +0,0 @@
-#ifndef __GUI_OLEDBMP_H
-#define __GUI_OLEDBMP_H
-
-#endif
